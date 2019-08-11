e208f835a85a1dfe13bc7105c331832167935c9f
Jorge Barbosa <jorgebarbosa_@outlook.com>
2019-08-11 18:04:39 -0400

    Configuração básica das views

---
+++ resources/views/alunos/detalhes.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+    detalhes
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,29 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('professores/index');
+});
+
+// Professores
+Route::get('professores', function () {
+    return view('professores/index');
+});
+Route::get('professor/{id}', function () {
+    return view('professores/detalhes');
+});
+Route::get('professor/editar/{id}', function () {
+    return view('professores/editar');
+});
+
+// Alunos
+
+Route::get('alunos', function () {
+    return view('alunos/index');
+});
+Route::get('aluno/{id}', function () {
+    return view('alunos/detalhes');
+});
+Route::get('alunos/editar/{id}', function () {
+    return view('alunos/editar');
 });
